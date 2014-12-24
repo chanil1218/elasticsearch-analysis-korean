2f2ebbe911089aaba6ea42257122ac9523a2846a
jhsbeat <jhsbeat@gmail.com>
2014-12-24 10:45:11 +0900

    Fixed version conflict issue of the Lucene Korean analyzer. IE., Updated for Elasticsearch 1.x

--- src/main/java/org/apache/solr/analysis/kr/KoreanTokenizerFactory.java
+++ src/main/java/org/apache/solr/analysis/kr/KoreanTokenizerFactory.java
@@ -1,27 +1,35 @@
 package org.apache.solr.analysis.kr;
 
 import java.io.Reader;
-
+import java.util.HashMap;
+import java.util.Map;
 
 import org.apache.lucene.analysis.Tokenizer;
 import org.apache.lucene.analysis.kr.KoreanTokenizer;
 import org.apache.lucene.analysis.util.TokenizerFactory;
+import org.apache.lucene.util.AttributeFactory;
 import org.apache.lucene.util.Version;
 
 public class KoreanTokenizerFactory extends TokenizerFactory {
 
 	private Version version;
-	
+
 	public KoreanTokenizerFactory() {
-		version = Version.LUCENE_42;
-	}
-	
-	public KoreanTokenizerFactory(Version v) {
-		version = v;
-	}
-	
-	public Tokenizer create(Reader input) {
-		return new KoreanTokenizer(version, input);
+		super(new HashMap<String, String>());
+		version = Version.LUCENE_4_9;
 	}
 
+	public KoreanTokenizerFactory(Version v) {
+		super(new HashMap<String, String>());
+		version = v;
+	}
+
+	protected KoreanTokenizerFactory(Map<String, String> args) {
+		super(args);
+	}
+
+	@Override
+	public Tokenizer create(AttributeFactory attributeFactory, Reader reader) {
+		return new KoreanTokenizer(version, attributeFactory, reader);
+	}
 }
