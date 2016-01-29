268c34874382fd0593b29750253dd96676228f71
Adriano Crestani <adrianocrestani@gmail.com>
2016-01-29 15:41:49 -0500

    adding support for ES 2.1.1

--- src/main/java/org/elasticsearch/index/analysis/KoreanTokenizerFactory.java
+++ src/main/java/org/elasticsearch/index/analysis/KoreanTokenizerFactory.java
@@ -1,19 +1,17 @@
 package org.elasticsearch.index.analysis;
 
-import java.io.Reader;
-
 import org.apache.lucene.analysis.Tokenizer;
 import org.apache.lucene.analysis.kr.KoreanTokenizer;
 import org.elasticsearch.common.inject.Inject;
-import org.elasticsearch.common.inject.assistedinject.Assisted;
 import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.index.Index;
-import org.elasticsearch.index.settings.IndexDynamicSettings;
+import org.elasticsearch.index.settings.IndexSettingsService;
 
 public class KoreanTokenizerFactory extends AbstractTokenizerFactory {
+
   @Inject
-  public KoreanTokenizerFactory(Index index, @IndexDynamicSettings Settings indexSettings, @Assisted String name, @Assisted Settings settings) {
-    super(index, indexSettings, name, settings);
+  public KoreanTokenizerFactory(Index index, IndexSettingsService indexSettings, String name, Settings settings) {
+    super(index, indexSettings.getSettings(), name, settings);
   }
 
   @Override
